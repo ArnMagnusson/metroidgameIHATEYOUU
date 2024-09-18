bf5b1d1d8ec9f7a16c2128bea050da6013164328
ArnMagnusson <birksanddahlmortensen@gmail.com>
2024-09-18 10:23:46 +0200

    Testing
    testing

--- movementcontroller.gd
+++ movementcontroller.gd
@@ -1,31 +1,37 @@
 extends CharacterBody2D
 
-
 const SPEED = 300.0
 const JUMP_VELOCITY = -400.0
 
-
 func _physics_process(delta):
-	# Add the gravity.
+	# Add the gravity. is on floor detects if its a flat surface on the button.
 	if not is_on_floor():
 		velocity += get_gravity() * delta
 
-	# Handle jump.
+	# Handle jump, checks if on floor and if value is true jump gets activated
 	if Input.is_action_just_pressed("ui_accept") and is_on_floor():
 		velocity.y = JUMP_VELOCITY
+	# if in air and releases space, set velocity to 0
 	if Input.is_action_just_released("ui_accept"):
 		velocity.y = 0
 
 	# Get the input direction and handle the movement/deceleration.
-	# As good practice, you should replace UI actions with custom gameplay actions.
 	var direction = Input.get_axis("left_A", "Right_D")
 	
-	#Flip sprite
-	if Input.is_action_just_pressed("Right_D"):
+	#Flip sprite, depending on direction
+	# if its above 0 its moving right if below zero moving left
+	if direction > 0:
 		$AnimatedSprite2D.flip_h = false
-	elif Input.is_action_just_pressed("left_A"):
+	if direction < 0:
 		$AnimatedSprite2D.flip_h = true
+
+	# Play animation when moving
+	if velocity.is_zero_approx():
+		$AnimatedSprite2D.play("Walk")
+	if direction == 0:
+		$AnimatedSprite2D.play("Idle")
 	
+	#Direction = movement
 	if direction:
 		velocity.x = direction * SPEED
 	else:
